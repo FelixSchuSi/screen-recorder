12b1227a352627f489a25bc80369a8942b1fc462
Felix Schulze Sindern <f.schulzesindern@web.de>
2024-02-29 22:37:47 +0100

    Allow microphone to be recorded

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const preview = document.getElementById("preview");
 const startButton = document.getElementById("startButton");
 /** @type {HTMLButtonElement} */
 const stopButton = document.getElementById("stopButton");
+/** @type {HTMLInputElement} */
+const recordMicCheckbox = document.getElementById("record-mic");
+
 /** @type {HTMLPreElement} */
 const logElement = document.getElementById("log");
 
@@ -12,7 +15,7 @@ const logElement = document.getElementById("log");
  * @param {string} msg
  */
 function log(msg) {
-  logElement.innerHTML += `${msg}\n`;
+    logElement.innerHTML += `${msg}\n`;
 }
 
 /**
@@ -22,83 +25,116 @@ function log(msg) {
  * @returns {Promise<void>} the recorded stream
  */
 function startRecording(mediaStream, writableStream) {
-  return new Promise((resolve, reject) => {
-    const recorder = new MediaRecorder(mediaStream);
-    recorder.ondataavailable = (event) => writableStream.write(event.data);
-    recorder.start();
-    recorder.onstop = resolve;
-    recorder.onerror = (event) => reject(event.name);
-  }).finally(() => writableStream.close());
+    return new Promise((resolve, reject) => {
+        const recorder = new MediaRecorder(mediaStream);
+        recorder.ondataavailable = (event) => writableStream.write(event.data);
+        recorder.start();
+        recorder.onstop = resolve;
+        recorder.onerror = (event) => reject(event.name);
+    }).finally(() => writableStream.close());
 }
 
 /**
- * @param {number} bytes
+ * Merges a Videostream containing Audio- and Videotracks with an
+ * Audiostream that only contains an Audiotrack.
+ * In the Outputstream you can hear both Audiotracks.
+ * @param {MediaStream} videoStream 
+ * @param {MediaStream} audioStream 
+ */
+function mergeVideoAndAudioStream(videoStream, audioStream) {
+    const audioContext = new AudioContext();
+
+    const mediaStreamDestination = audioContext.createMediaStreamDestination();
+    audioContext.createMediaStreamSource(videoStream).connect(mediaStreamDestination);
+    audioContext.createMediaStreamSource(audioStream).connect(mediaStreamDestination);
+
+    const mergedStream = new MediaStream();
+    videoStream.getVideoTracks().forEach(track => {
+        mergedStream.addTrack(track);
+    });
+    mediaStreamDestination.stream.getAudioTracks().forEach(track => {
+        mergedStream.addTrack(track);
+    });
+    return mergedStream;
+}
+
+/**
+ * Formats the size of a file using IEC prefix names (e. g. KiB, MiB, etc.)
+ * @param {number} bytes number of bytes
  * @returns {string} formatted file size
  */
 function formatFileSizeIEC(bytes) {
-  const binaryKilo = 1024;
-  const unit = (Math.log(bytes) / Math.log(binaryKilo)) | 0;
-  const value = (bytes / Math.pow(binaryKilo, unit)).toFixed(2);
-  const postfix = unit ? "KMGTPEZY"[unit - 1] + "iB" : "Bytes";
-  return `${value} ${postfix}`;
+    const binaryKilo = 1024;
+    const unit = (Math.log(bytes) / Math.log(binaryKilo)) | 0;
+    const value = (bytes / Math.pow(binaryKilo, unit)).toFixed(2);
+    const postfix = unit ? "KMGTPEZY"[unit - 1] + "iB" : "Bytes";
+    return `${value} ${postfix}`;
 }
 
 /**
- * @param {number} milliseconds
+ * Formats a duration of time.
+ * @param {number} milliseconds 
  * @returns {string} formatted duration
  */
 function formatDuration(milliseconds) {
-  if (milliseconds < 1000) {
-    return `${milliseconds} ms`;
-  }
-  const seconds = milliseconds / 1000;
-  if (seconds < 60) {
-    return `${seconds} seconds`;
-  }
-  const minutes = seconds / 60;
-  return `${minutes} minutes and ${seconds % 60} seconds`;
+    if (milliseconds < 1000) {
+        return `${milliseconds} ms`;
+    }
+    const seconds = milliseconds / 1000;
+    if (seconds < 60) {
+        return `${seconds} seconds`;
+    }
+    const minutes = seconds / 60;
+    return `${minutes} minutes and ${seconds % 60} seconds`;
 }
 
 async function main() {
-  /** @type {FileSystemHandle} */
-  const fileHandle = await window.showSaveFilePicker({
-    startIn: "downloads",
-    suggestedName: new Date().toISOString() + ".webm",
-    accept: { "video/webm": [".webm"] },
-  });
-  /** @type {WritableStream<Blob>} */
-  const writableStream = await fileHandle.createWritable();
-  preview.srcObject = await navigator.mediaDevices.getDisplayMedia({
-    video: {
-      displaySurface: "monitor",
-    },
-    audio: true,
-  });
+    /** @type {FileSystemHandle} */
+    const fileHandle = await window.showSaveFilePicker({
+        startIn: "downloads",
+        suggestedName: new Date().toISOString() + ".webm",
+        accept: { "video/webm": [".webm"] },
+    });
+    /** @type {WritableStream<Blob>} */
+    const writableStream = await fileHandle.createWritable();
+    const videoStream = await navigator.mediaDevices.getDisplayMedia({
+        video: {
+            displaySurface: "monitor",
+        },
+        audio: true,
+    });
 
-  await new Promise((resolve) => (preview.onplaying = resolve));
+    if (recordMicCheckbox.checked) {
+        const audioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        preview.srcObject = mergeVideoAndAudioStream(videoStream, audioStream);
+    } else {
+        preview.srcObject = videoStream;
+    }
 
-  window.onbeforeunload = (event) => event.preventDefault();
-  startButton.classList.add("invisible");
-  stopButton.classList.remove("invisible");
-  const recordingStart = performance.now();
-  log(`recording started 🔴`);
-  await startRecording(preview.captureStream(), writableStream);
+    await new Promise((resolve) => (preview.onplaying = resolve));
 
-  window.onbeforeunload = () => {};
-  preview.srcObject = null;
+    window.onbeforeunload = (event) => event.preventDefault();
+    startButton.classList.add("invisible");
+    stopButton.classList.remove("invisible");
+    const recordingStart = performance.now();
+    log(`recording started 🔴`);
+    await startRecording(preview.captureStream(), writableStream);
 
-  preview.src = URL.createObjectURL(fileHandle);
+    window.onbeforeunload = () => { };
+    preview.srcObject = null;
 
-  stopButton.classList.add("invisible");
+    preview.src = URL.createObjectURL(fileHandle);
 
-  log(`recording stopped ⬛`);
-  log(`Successfully recorded:`);
-  log(`  ${fileHandle.name}`);
-  log(`  ${formatDuration(performance.now() - recordingStart)}`);
-  log(`  ${formatFileSizeIEC(fileHandle.size)} of ${fileHandle.type} media.`);
+    stopButton.classList.add("invisible");
+
+    log(`recording stopped ⬛`);
+    log(`Successfully recorded:`);
+    log(`  ${fileHandle.name}`);
+    log(`  ${formatDuration(performance.now() - recordingStart)}`);
+    log(`  ${formatFileSizeIEC(fileHandle.size)} of ${fileHandle.type} media.`);
 }
 
 startButton.addEventListener("click", () => main().catch((e) => log(e)));
 stopButton.addEventListener("click", () =>
-  preview.srcObject.getTracks().forEach((track) => track.stop())
+    preview.srcObject.getTracks().forEach((track) => track.stop())
 );
