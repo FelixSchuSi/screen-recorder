a30450d9de00617ef5f0c64d2ae01603c1bebf5f
Felix Schulze Sindern <f.schulzesindern@web.de>
2025-02-16 13:03:41 +0100

    Dont use filesystem api

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-/** @type {HTMLVideoElement} */
-const preview = document.getElementById("preview");
-/** @type {HTMLButtonElement} */
-const startButton = document.getElementById("startButton");
-/** @type {HTMLButtonElement} */
-const stopButton = document.getElementById("stopButton");
-/** @type {HTMLInputElement} */
-const recordMicCheckbox = document.getElementById("record-mic");
-/** @type {HTMLInputElement} */
-const labelElement = document.getElementById("record-mic-label");
-/** @type {HTMLPreElement} */
-const logElement = document.getElementById("log");
+
+const preview = /** @type {HTMLVideoElement} */ (document.getElementById("preview"));
+const startButton =/** @type {HTMLButtonElement} */ (document.getElementById("startButton"));
+const stopButton = /** @type {HTMLButtonElement} */ (document.getElementById("stopButton"));
+const recordMicCheckbox = /** @type {HTMLInputElement} */ (document.getElementById("record-mic"));
+const labelElement = /** @type {HTMLInputElement} */ (document.getElementById("record-mic-label"));
+const logElement = /** @type {HTMLPreElement} */ (document.getElementById("log"));
 /** @type {number} */
 let recordingStart;
 
@@ -22,18 +17,18 @@ function log(msg) {
 }
 
 /**
- * Records a Mediastream and writes it to a WritableStream.
+ * Records a Mediastream and writes it to a array of Blobs.
  * @param {MediaStream} mediaStream
- * @param {WritableStream<Blob>} writableStream
+ * @param {Blob[]} chunks
  */
-function record(mediaStream, writableStream) {
+function record(mediaStream, chunks) {
     return new Promise((resolve, reject) => {
         const recorder = new MediaRecorder(mediaStream);
-        recorder.ondataavailable = (event) => writableStream.write(event.data);
+        recorder.ondataavailable = (event) => chunks.push(event.data);;
         recorder.onstop = () => resolve();
-        recorder.onerror = (event) => reject(event.name);
+        recorder.onerror = (event) => reject(event);
         recorder.start();
-    }).finally(() => writableStream.close());
+    });
 }
 
 /**
@@ -90,11 +85,11 @@ function formatDuration(milliseconds) {
         return `${seconds.toFixed(2)} seconds`;
     }
     const minutes = seconds / 60;
-    return `${parseInt(minutes)} minutes and ${(seconds % 60).toFixed(2)} seconds`;
+    return `${Math.trunc(minutes)} minutes and ${(seconds % 60).toFixed(2)} seconds`;
 }
 
 /**
- * @returns {number}
+ * @returns {{ recordingStart: number; }}
  */
 function onRecordStart() {
     window.onbeforeunload = (event) => event.preventDefault();
@@ -108,32 +103,33 @@ function onRecordStart() {
 }
 
 /**
- * @param {{fileHandle: FileSystemFileHandle, recordingStart: number}} param 
+ * @param {{recordingStart: number, chunks: Blob[]}} param 
  */
-async function onRecordFinish({ fileHandle, recordingStart }) {
+async function onRecordFinish({  recordingStart, chunks }) {
     window.onbeforeunload = () => { };
-    preview.srcObject?.getTracks()?.forEach((track) => track.stop());
-
+    (/** @type {MediaStream} */  (preview.srcObject))?.getTracks()?.forEach((track) => track.stop());
     preview.srcObject = null;
-    const file = await fileHandle.getFile();
-    preview.src = URL.createObjectURL(file);
+    const blob = new Blob(chunks, {
+        type: "video/webm"
+    });
+    preview.src = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = preview.src;
+    a.download = `${new Date().toISOString()}.webm`;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+
+
     stopButton.classList.add("invisible");
     log(`recording stopped ⬛`);
     log(`Successfully recorded:`);
-    log(`  ${fileHandle.name}`);
     log(`  ${formatDuration(performance.now() - recordingStart)}`);
-    log(`  ${formatFileSizeIEC(file.size)} of ${file.type} media.`);
 }
 
 async function main() {
-    /** @type {FileSystemHandle} */
-    const fileHandle = await window.showSaveFilePicker({
-        startIn: "downloads",
-        suggestedName: new Date().toISOString() + ".webm",
-        accept: { "video/webm": [".webm"] },
-    });
-    /** @type {WritableStream<Blob>} */
-    const writableStream = await fileHandle.createWritable();
     const videoStream = await navigator.mediaDevices.getDisplayMedia({
         video: {
             displaySurface: "monitor",
@@ -156,10 +152,10 @@ async function main() {
     // the stream ends silently.
     // If that happens, we force the recording to stop.
     const intervallId = setInterval(() => {
-        const videoTracks = preview.srcObject?.getVideoTracks();
+        const videoTracks = (/** @type {MediaStream} */  (preview.srcObject))?.getVideoTracks();
         const videoTrackEnded = !!videoTracks?.find(t => t.readyState === "ended");
         if (videoTrackEnded || videoTracks === undefined || videoTracks === null) {
-            preview.srcObject?.getTracks()?.forEach((track) => track.stop());
+            (/** @type {MediaStream} */  (preview.srcObject))?.getTracks()?.forEach((track) => track.stop());
             audioStream?.getTracks()?.forEach((track) => track.stop());
             videoStream?.getTracks()?.forEach((track) => track.stop());
             clearInterval(intervallId);
@@ -167,11 +163,14 @@ async function main() {
         }
     }, 1000);
 
-    await record(preview.captureStream(), writableStream);
-    await onRecordFinish({ recordingStart, fileHandle });
+    /** @type {Blob[]} */
+    const chunks = [];
+    // @ts-ignore
+    await record(preview.captureStream(), chunks);
+    await onRecordFinish({ recordingStart, chunks });
 }
 
 startButton.addEventListener("click", () => main().catch((e) => log(e)));
 stopButton.addEventListener("click", () =>
-    preview.srcObject.getTracks()?.forEach((track) => track.stop())
+    (/** @type {MediaStream} */  (preview.srcObject))?.getTracks()?.forEach((track) => track.stop())
 );
